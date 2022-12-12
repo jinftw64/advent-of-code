d0b31978ddb1484748b00477074ff50e30e0a75d
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2022-12-12 16:06:21 -0500

    Added puzzle input text file and variables names in py file.

--- 2022/day_03/day_03.py
+++ 2022/day_03/day_03.py
@@ -5,7 +5,7 @@ import string
 alphabet_lower = list(string.ascii_lowercase)
 alphabet_upper = list(string.ascii_uppercase)
 
-alphabet_upper = list(enumerate(alphabet_upper, 1))
-alphabet_lower = list(enumerate(alphabet_lower, 27))
+alphabet_lower = list(enumerate(alphabet_lower, 1))
+alphabet_upper = list(enumerate(alphabet_upper, 27))
 
-
+print(alphabet_lower)
