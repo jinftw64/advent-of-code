420583ac9ce81b84f9e794007c400cf19ffc089c
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2022-12-12 15:57:57 -0500

    Started Day 3 Part 1

--- 2022/day_03/day_03.py
+++ 2022/day_03/day_03.py
@@ -1,2 +1,11 @@
 # Advent of Code 2022
 # Day 3 Part 1
+
+import string
+alphabet_lower = list(string.ascii_lowercase)
+alphabet_upper = list(string.ascii_uppercase)
+
+alphabet_upper = list(enumerate(alphabet_upper, 1))
+alphabet_lower = list(enumerate(alphabet_lower, 27))
+
+
