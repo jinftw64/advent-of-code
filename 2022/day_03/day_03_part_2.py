d10c072e31c97c67c9fd6efa927ffeac68954a62
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2022-12-14 23:51:52 -0500

    Created function that interates and creates a list of 3 sets

--- 2022/day_03/day_03_part_2.py
+++ 2022/day_03/day_03_part_2.py
@@ -5,4 +5,16 @@ import string
 alphabet_lower = list(string.ascii_lowercase)
 alphabet_upper = list(string.ascii_uppercase)
 
+list_3_lines = []
 
+with open('text.txt') as file:
+    lines = file.read().splitlines()
+
+# remove print debug function and 
+# implement intersect method 
+for line in lines:
+    if len(list_3_lines) == 3:
+        print(f"This is a set {list_3_lines}")
+        list_3_lines = []
+    else:
+        list_3_lines.append(set(line))
