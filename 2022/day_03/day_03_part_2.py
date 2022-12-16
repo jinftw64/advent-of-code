ec71fd49d9bb99d20856f0c384695ca529448103
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2022-12-16 16:23:54 -0500

    Added enumerate functions.

--- 2022/day_03/day_03_part_2.py
+++ 2022/day_03/day_03_part_2.py
@@ -5,7 +5,18 @@ import string
 alphabet_lower = list(string.ascii_lowercase)
 alphabet_upper = list(string.ascii_uppercase)
 
+def lower_convert(character):
+    if character.islower():
+        for value, letter in enumerate(alphabet_lower, 1):
+            if character == letter:
+                return value
+    else:
+        for value, letter in enumerate(alphabet_upper, 27):
+            if character == letter:
+                return value
+
 list_3_lines = []
+total = 0
 
 with open('text.txt') as file:
     lines = file.read().splitlines()
@@ -14,7 +25,10 @@ with open('text.txt') as file:
 # implement intersect method 
 for line in lines:
     if len(list_3_lines) == 3:
-        print(f"This is a set {list_3_lines}")
+        common_letter = list_3_lines[0].intersection(list_3_lines[1], list_3_lines[2])
+        total += lower_convert(common_letter[0])
         list_3_lines = []
     else:
         list_3_lines.append(set(line))
+
+print(f"The total is: {total}")
