138f77e539732463cf0537c828632926ed40a4d8
jinftw64 <103798287+jinftw64@users.noreply.github.com>
2022-12-28 22:43:27 -0500

    Attempt at string parser.

--- 2022/day_05/day_05.py
+++ 2022/day_05/day_05.py
@@ -2,8 +2,15 @@
 # Day 5 Part 1
 
 stacks = []
+row = []
 
 with open("stacks.txt") as file:
     lines = file.read().splitlines()
     for line in lines:
-        pass
+        row = []
+        for i in range(9):
+            row.append(line[(i * 3):(i * 3 + 4)])
+        stacks.append(row)
+
+for i in stacks:
+    print(i)
